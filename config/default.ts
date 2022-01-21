639ed207e0c73bb96f8c0fdb1fd3ecb6254e3917
Yasir Idriz <yasiridriz@Yasirs-MacBook-Pro.local>
2022-01-21 21:53:44 +0100

    cors

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,5 @@
 export default {
-  corsOrigin:["http://192.168.1.122:3000", 'http://localhost:3000', 'http://scattergories.netlify.app'],
+  corsOrigin:"http://192.168.1.122:3000",
   port: process.env.PORT,
   host: process.env.HOST,
 };
