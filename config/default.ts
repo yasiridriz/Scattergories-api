e51c99ad700daa1dc7f13872577efca038af6b7e
Yasir Idriz <yasiridriz@Yasirs-MacBook-Pro.local>
2022-01-21 21:46:44 +0100

    initial

---
+++ config/default.ts
@@ -0,0 +1,5 @@
+export default {
+  corsOrigin:["http://192.168.1.122:3000", 'http://localhost:3000', 'http://scattergories.netlify.app'],
+  port: process.env.PORT,
+  host: process.env.HOST,
+};
---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import config from "config";
+import logger from "./utils/logger";
+import { version } from "../package.json";
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+import socket from "./socket";
+
+const port = config.get<number>("port");
+const host = config.get<string>("host");
+const corsOrigin = config.get<string>("corsOrigin");
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: corsOrigin,
+    credentials: true,
+  },
+});
+
+app.get("/", (_, res) =>
+  res.send(`Server is up and running version ${version}`)
+);
+
+httpServer.listen(port, host, () => {
+  logger.info(`🚀 Server version ${version} is listening 🚀`);
+  logger.info(`${host}:${port}`);
+
+  socket({ io });
+});
